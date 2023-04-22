eaa20044ec0df058a39d08a6e41ce6e2d4d93737
Trevor McLaurine <trevor.mclaurine@gmail.com>
2023-04-22 08:28:09 -0500

    adding registration

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require ('express')
+const mongoose = require('mongoose');
+require('dotenv').config({path:__dirname+'/.env'});
+const Customer = require('./models/customer')
+
+const PORT = process.env.PORT || 3000; 
+const MONGO = process.env.MONGO_URI;
 
 const app = express(); 
+
+app.use(express.json());
 
 app.set('view engine', 'ejs')
 
@@ -23,3 +31,33 @@ app.get('/boarding', (req, res) => {
 app.get('/training', (req, res) => {
     res.render('training')
 })
+
+app.get('/registration', (req, res) => {
+    res.render('registration')
+})
+
+app.post('/registration', (req, res) => {
+
+    const { customerID, email } = req.body;
+
+    try {
+        const customer = Customer.create({ customerID, email })
+        res.status(200).json(customer);
+        res.render('/');
+    }
+    catch (error)
+    {
+        res.status(400).json({ error: error.message })
+    }
+    
+})
+
+mongoose.connect(MONGO)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log('Port', process.env.PORT);
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    })
---
+++ models/customer.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+let customerSchema = new mongoose.Schema({
+    customerID: 
+    { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    email: 
+    {
+        type: String, 
+        required: true,
+        unique: true
+    }
+});
+
+module.exports = mongoose.model('Customer', customerSchema);
